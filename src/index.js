b383b3e588626565391128e73c76484f63449708
hfenelsoftllc <hfenelsoftllc@gmail.com>
2024-03-14 00:38:48 -0400

    Secure db connection with AWS and policies

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 const serverless = require("serverless-http");
 const express = require("express");
+const AWS = require("aws-sdk")
 const {neon, neonConfig} = require("@neondatabase/serverless");
 
+const AWS_REGION ="us-east-1"
+const STAGE = process.env.STAGE || 'prod'
+
 const app = express();
+
+const ssm = new AWS.SSM({region: AWS_REGION})
+
+const DATABASE_URL_SSM_PARAM =`/serverless-node-api/${STAGE}/prod/database-url`
 
 async function dbClient(){
   //for http connections
   // non pooling
+  const paramStoreData = await ssm.getParameter({
+    Name: DATABASE_URL_SSM_PARAM,
+    WithDecryption: true
+  }).promise()
+
   neonConfig.fetchConnectionCache = true
-  const sql = neon(process.env.DATABASE_URL);
+  const sql = neon(paramStoreData.Parameter.Value);
   return sql
 }
 
 
 app.get("/", async(req, res, next) => {
+  console.log(process.env.DEBUG)
   const sql = await dbClient()
   const [results] = await sql`select now();`
   return res.status(200).json({
